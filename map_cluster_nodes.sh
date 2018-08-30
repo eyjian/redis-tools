100284d6c2cf308014bc3dd0167ec9bda3b70fff
eyjian <eyjian@live.com>
2018-08-30 21:56:53 +0800

    如果同一对master和slave的IP相同，标星

--- map_cluster_nodes.sh
+++ map_cluster_nodes.sh
@@ -51,8 +51,8 @@ fi
 declare -A master_map=()
 # slave映表，key为master的id，value为slave的“ip:port”
 declare -A slave_map=()
-master_node_str=
-master_slave_map_str=
+master_nodes_str=
+master_slave_maps_str=
 
 # 找出所有master
 masters=`$REDIS_CLI -h $REDIS_IP -p $REDIS_PORT CLUSTER NODES | awk -F[\ \@] '/master/{ printf("%s,%s\n",$1,$2); }' | sort`
@@ -61,10 +61,10 @@ do
     eval $(echo $master | awk -F[,] '{ printf("master_id=%s\nmaster_node=%s\n",$1,$2); }')
     
     master_map[$master_id]=$master_node    
-    if test -z "$master_node_str"; then
-        master_node_str="$master_node"
+    if test -z "$master_nodes_str"; then
+        master_nodes_str="$master_node|$master_id"
     else
-        master_node_str="$master_node,$master_node_str"
+        master_nodes_str="$master_node|$master_id,$master_nodes_str"
     fi
 done
 
@@ -81,37 +81,45 @@ do
     master_node=${master_map[$key]}
     slave_node=${slave_map[$key]}
 
-    if test -z "$master_slave_map_str"; then
-        master_slave_map_str="$slave_node#$master_node"
+    if test -z "$master_slave_maps_str"; then
+        master_slave_maps_str="$slave_node|$master_node"
     else
-        master_slave_map_str="$slave_node#$master_node,$master_slave_map_str"
+        master_slave_maps_str="$slave_node|$master_node,$master_slave_maps_str"
     fi
 done
 
 # 显示所有master
 index=1
-master_nodes=`echo "$master_node_str" | tr ',' '\n' | sort`
-for maste_node in $master_nodes;
+master_nodes_str=`echo "$master_nodes_str" | tr ',' '\n' | sort`
+for master_node_str in $master_nodes_str;
 do
-    printf "[%02d][MASTER] %s\n" $index "$maste_node"
+    eval $(echo "$master_node_str" | awk -F[\|] '{ printf("master_node=%s\nmaster_id=%s\n", $1, $2); }')
+    printf "[%02d][MASTER]  %-20s \033[0;32;31m%s\033[m\n" $index "$master_node" "$master_id"
     index=$((++index))
 done
 
 # 显示所有slave到master的映射
 index=1
 echo ""
-master_slave_nodes=`echo "$master_slave_map_str" | tr ',' '\n' | sort`
-for master_slave_node in $master_slave_nodes;
+master_slave_maps_str=`echo "$master_slave_maps_str" | tr ',' '\n' | sort`
+for master_slave_map_str in $master_slave_maps_str;
 do
-    line=`echo "$master_slave_node" | sed 's/#/  =>  /g'`
-
-    n=$(($index % 2))
-    if test $n -eq 1; then
-        printf "[%02d][SLAVE=>MASTER]  \033[1;33m%s\033[m\n" $index "$line"
-    else
-        printf "[%02d][SLAVE=>MASTER]  %s\n" $index "$line"
+    eval $(echo "$master_slave_map_str" | awk -F[\|] '{ printf("slave_node=%s\nmaster_node=%s\n", $1, $2); }')
+    eval $(echo "$slave_node" | awk -F[\:] '{ printf("slave_node_ip=%s\nslave_node_port=%s\n", $1, $2); }')
+    eval $(echo "$master_node" | awk -F[\:] '{ printf("master_node_ip=%s\nmaster_node_port=%s\n", $1, $2); }')
+    
+    tag=
+    if test ! -z "$slave_node_ip" -a "$slave_node_ip" = "$master_node_ip"; then
+        tag="  (*)"
     fi
 
+    n=$(($index % 2))
+    if test $n -eq 0; then
+        printf "[%02d][SLAVE=>MASTER] \033[1;33m%21s\033[m  =>  \033[1;33m%s\033[m%s\n" $index $slave_node $master_node $tag
+    else
+        printf "[%02d][SLAVE=>MASTER] %21s  =>  %s%s\n" $index $slave_node $master_node $tag
+    fi
+    
     index=$((++index))
 done
 
