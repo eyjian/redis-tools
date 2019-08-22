81954313489069b2028fd39d1292b66c9852970b
eyjian <eyjian@live.com>
2019-08-22 14:31:43 +0800

    Optimize

--- clear_redis_cluster.sh
+++ clear_redis_cluster.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Writed by yijian on 2018/8/20
 # Batch to clear all nodes using FLUSHALL command
-
+# 用来清空一个redis集群中的所有数据，要求 FLUSHALL 命令可用，
+# 如果在 redis.conf 中使用 rename 改名了 FLUSHALL，则不能执行本脚本。
 REDIS_CLI=${REDIS_CLI:-redis-cli}
 REDIS_IP=${REDIS_IP:-127.0.0.1}
 REDIS_PORT=${REDIS_PORT:-6379}
 
 function usage()
 {
-    echo "usage: clear_redis_cluster.sh redis_node"    
-    echo "example: clear_redis_cluster.sh 127.0.0.1:6379"    
+    echo "Usage: clear_redis_cluster.sh a_redis_node_of_cluster"    
+    echo "Example: clear_redis_cluster.sh 127.0.0.1:6379"    
 }
 
 # with a parameter: single redis node
@@ -20,25 +21,27 @@ fi
 
 eval $(echo "$1" | awk -F[\:] '{ printf("REDIS_IP=%s\nREDIS_PORT=%s\n",$1,$2) }')
 if test -z "$REDIS_IP" -o -z "$REDIS_PORT"; then
-    echo "parameter error"
+    echo "Parameter error: \`$1\`."
     usage
     exit 1
 fi
 
 # 确保redis-cli可用
-echo "checking \`$REDIS_CLI\` ..."
+echo "Checking \`redis-cli\` ..."
 which "$REDIS_CLI" > /dev/null 2>&1
 if test $? -ne 0; then
-    echo "\`redis-cli\` not exists or not executable"
+    echo "Command \`redis-cli\` is not exists or not executable."
+    echo "You can set environment variable \`REDIS_CLI\` to point to the redis-cli."
+    echo "Example: export REDIS_CLI=/usr/local/bin/redis-cli"
     exit 1
 fi
 
 redis_nodes=`redis-cli -h $REDIS_IP -p $REDIS_PORT cluster nodes | awk -F[\ \:\@] '!/ERR/{ printf("%s:%s\n",$2,$3); }'`
 if test -z "$redis_nodes"; then
-    # standlone
+    # Standlone（非集群）
     $REDIS_CLI -h $REDIS_IP -p $REDIS_PORT FLUSHALL
 else
-    # cluster
+    # Cluster（集群）
     for redis_node in $redis_nodes;
     do
         if test ! -z "$redis_node"; then
@@ -46,7 +49,7 @@ else
 
             if test ! -z "$redis_node_ip" -a ! -z "$redis_node_port"; then
                 # clear
-                echo -e "clearing \033[1;33m${redis_node_ip}:${redis_node_port}\033[m ..."
+                echo -e "Clearing \033[1;33m${redis_node_ip}:${redis_node_port}\033[m ..."
                 result=`$REDIS_CLI -h $redis_node_ip -p $redis_node_port FLUSHALL`
 
                 if test ! -z "$result"; then
