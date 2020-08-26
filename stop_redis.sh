d2fed8486d3791d529ec03c786292bacfac8706a
eyjian <eyjian@live.com>
2020-08-26 20:46:02 +0800

    新增停止指定端口号redis-server进程的工具

---
+++ stop_redis.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Writed by yijian on 2020/8/26
+# 停止监控在指定端口的 redis-server 进程
+# 执行依赖 netstat 和 awk 两个命令
+#
+# 带一个参数：
+# 1）参数1：redis-server 的监听端口号
+
+# 使用帮助函数
+function usage()
+{
+  echo "Usage: `basename $0` redis-port"
+  echo "Example: `basename $0` 6379"
+}
+
+# 参数检查
+if test $# -ne 1; then
+  usage
+  exit 1
+fi
+
+# 参数指定的 redis-server 监听端口
+REDIS_PORT=$1
+
+# 取得 redis-server 进程ID
+# 命令 ps 输出的时间格式有两种：“7月17”和“20:42”，所以端口所在字段有区别：
+pid=`ps -f -C redis-server | awk -v port=$REDIS_PORT -F'[ :]*' '{ if ($12==port || $13==port) print $2 }'`
+if test -z "$pid"; then
+  echo "redis-server[$REDIS_PORT] is not running"
+elif test $pid -eq 0; then
+  echo "redis-server[$REDIS_PORT]'s pid is 0"
+else
+  echo "kill $pid (redis-server[$REDIS_PORT])"
+  #kill -0 $pid
+  kill $pid
+fi
